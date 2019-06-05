d856f8f8e044ac22653c6eba307e1a8b6e43a29f
Avi Tuby <avi@ivory-sw.com>
2019-06-05 11:31:45 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by DAVID AVI TUBI on 05/06/2019.
+//  Copyright © 2019 DAVID AVI TUBI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
